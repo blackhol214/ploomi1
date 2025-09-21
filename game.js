d8f13c631b0743879dfc7bf4f84ec1d66f8d5938
Mathias Fernandez <mathiasfernandezgil@gmail.com>
2025-09-21 18:40:41 -0500

    Enhance TitleScene and GameScene visuals and controls

--- game.js
+++ game.js
@@ -8,17 +8,17 @@ class TitleScene extends Phaser.Scene {
         this.cameras.main.setBackgroundColor('#000000');
 
         // White title text
-        this.add.text(400, 200, 'Ploomi and The Cosmic Core', {
-            fontSize: '32px',
+        this.add.text(400, 200, 'PLOOMI AND THE COSMIC CORE', {
+            fontSize: '50px',
             color: '#ffffff',
-            fontFamily: 'Courier New'
+            fontFamily: 'Futura Condensed ExtraBold'
         }).setOrigin(0.5);
 
         // Start button
         const startButton = this.add.text(400, 350, 'Start Plooming!', {
-            fontSize: '24px',
+            fontSize: '40px',
             color: '#ffffff',
-            fontFamily: 'Courier New'
+            fontFamily: 'Futura Condensed ExtraBold'
         }).setOrigin(0.5).setInteractive();
 
         startButton.on('pointerdown', () => {
@@ -26,7 +26,7 @@ class TitleScene extends Phaser.Scene {
         });
 
         startButton.on('pointerover', () => {
-            startButton.setColor('#ffff00'); // Yellow on hover
+            startButton.setColor('#0055ffff'); // Yellow on hover
         });
 
         startButton.on('pointerout', () => {
@@ -41,21 +41,54 @@ class GameScene extends Phaser.Scene {
     }
 
     preload() {
-        // Generate Ploomi texture
+        // Generate Ploomi textures
+        // Idle
         let graphics = this.add.graphics();
         graphics.fillStyle(0x0000ff); // blue
         graphics.fillRect(0, 0, 32, 32);
-        // eyes
         graphics.fillStyle(0x000000);
-        graphics.fillRect(7, 7, 5, 5);
-        graphics.fillRect(20, 7, 5, 5);
-        graphics.generateTexture('ploomi', 32, 32);
+        graphics.fillRect(7, 7, 6, 6);
+        graphics.fillRect(19, 7, 6, 6);
+        graphics.generateTexture('ploomi_idle', 32, 32);
+        graphics.destroy();
+
+        // Left
+        graphics = this.add.graphics();
+        graphics.fillStyle(0x0000ff);
+        graphics.fillRect(0, 0, 32, 32);
+        graphics.fillStyle(0x000000);
+        graphics.fillRect(5, 7, 6, 6);
+        graphics.fillRect(17, 7, 6, 6);
+        graphics.generateTexture('ploomi_left', 32, 32);
+        graphics.destroy();
+
+        // Right
+        graphics = this.add.graphics();
+        graphics.fillStyle(0x0000ff);
+        graphics.fillRect(0, 0, 32, 32);
+        graphics.fillStyle(0x000000);
+        graphics.fillRect(9, 7, 6, 6);
+        graphics.fillRect(21, 7, 6, 6);
+        graphics.generateTexture('ploomi_right', 32, 32);
+        graphics.destroy();
+
+        // Heart texture
+        graphics = this.add.graphics();
+        graphics.fillStyle(0xff0000);
+        // Simple heart shape
+        graphics.fillRect(6, 10, 4, 4);
+        graphics.fillRect(4, 8, 8, 4);
+        graphics.fillRect(2, 6, 12, 4);
+        graphics.fillRect(0, 4, 16, 4);
+        graphics.fillRect(2, 2, 12, 4);
+        graphics.fillRect(4, 0, 8, 4);
+        graphics.generateTexture('heart', 16, 16);
         graphics.destroy();
     }
 
     create() {
         // Background
-        this.cameras.main.setBackgroundColor('#000000');
+        this.cameras.main.setBackgroundColor('#0a008fff');
 
         // Health
         this.health = 3;
@@ -79,10 +112,10 @@ class GameScene extends Phaser.Scene {
         this.platforms.add(plat3);
 
         // Player
-        this.player = this.physics.add.sprite(100, 450, 'ploomi');
+        this.player = this.physics.add.sprite(100, 450, 'ploomi_idle');
         this.player.setCollideWorldBounds(true);
         this.player.setBounce(0.2);
-        this.player.setDragX(300);
+        this.player.setDragX(350);
 
         // Goal
         this.goal = this.add.rectangle(750, 200, 32, 32, 0xff0000);
@@ -94,19 +127,33 @@ class GameScene extends Phaser.Scene {
 
         // Input
         this.cursors = this.input.keyboard.createCursorKeys();
+        this.zKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Z);
     }
 
     update() {
         // Controls
         this.player.setAccelerationX(0);
         if (this.cursors.left.isDown) {
-            this.player.setAccelerationX(-200);
+            this.player.setAccelerationX(-350);
         } else if (this.cursors.right.isDown) {
-            this.player.setAccelerationX(200);
+            let speed = 350;
+            if (this.zKey.isDown) {
+                speed = 700;
+            }
+            this.player.setAccelerationX(speed);
         }
 
         if (this.cursors.up.isDown && this.player.body.touching.down) {
             this.player.setVelocityY(-330);
+        }
+
+        // Update eye direction
+        if (this.player.body.velocity.x > 10) {
+            this.player.setTexture('ploomi_right');
+        } else if (this.player.body.velocity.x < -10) {
+            this.player.setTexture('ploomi_left');
+        } else {
+            this.player.setTexture('ploomi_idle');
         }
 
         // Check if player falls off
